33a27805aa2644afc569015b594e1de56f274b7c
wsrtka <witold.serwatka@pm.me>
2020-06-27 13:25:10 +0200

    added server loop

--- Chapter1/netcat.py
+++ Chapter1/netcat.py
@@ -67,8 +67,20 @@ def main():
 
 
     def server_loop():
+        global target
 
-        
+        if not len(target):
+            target = '0.0.0.0'
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((target, port))
+        server.listen(5)
+
+        while True:
+            client_socket, addr = server.accept()
+
+            client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+            client_thread.start()
 
 
     global listen
