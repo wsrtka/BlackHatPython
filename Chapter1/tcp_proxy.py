118bfb5e6f23cf031aab4b73a53630d124abad94
wsrtka <witold.serwatka@pm.me>
2020-06-27 16:20:57 +0200

    added hexdump

--- Chapter1/tcp_proxy.py
+++ Chapter1/tcp_proxy.py
@@ -81,7 +81,19 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
                 break
 
 
-def 
+def hexdump(src, length=16):
+    
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+
+    for i in range(0, len(src), length):
+
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+        result.append(b'%04X %-*s %s' % (i, length*(digits + 1), hexa, text))
+
+    print(b'\n'.join(result))
 
 
 def main():
