5f60153d41a61ba6a531052eedc86ac4a30a1ac5
wsrtka <witold.serwatka@pm.me>
2020-06-28 12:34:12 +0200

    added ip header sniffer

---
+++ Chapter2/ip_header_decoder.py
@@ -0,0 +1,71 @@
+import socket
+import os
+import struct
+from ctypes import *
+
+
+# host ip to listen on
+host = '192.168.1.114'
+
+
+class IP(Structure):
+
+    # define the C ip struct using the ctypes library
+    _fields_ = [
+        ('ihl', c_ubyte, 4),
+        ('version', c_ubyte, 4),
+        ('tos', c_ubyte),
+        ('len', c_ushort),
+        ('id', c_ushort),
+        ('offset', c_ushort),
+        ('ttl', c_ubyte),
+        ('protocol_num', c_ubyte),
+        ('sum', c_ushort),
+        ('src', c_uint32),
+        ('dst', c_uint32),
+    ]
+
+    def __new__(self, socket_buffer=None):
+        
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+
+        # mapping protocol numbers to their names
+        self.protocol_map = {1: 'ICMP', 6: 'TCP', 17: 'UDP'}
+
+        # changing ip addresses from 32-bit packed format to dotted quad-string format
+        self.src_address = socket.inet_ntoa(struct.pack('@I', self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack('@I', self.dst))
+
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
+
+if os.name == 'nt':
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer.bind((host, 0))
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# if on Windows, switch unlimited mode on
+if os.name == 'nt':
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+try:
+    while True:
+
+        raw_buffer = sniffer.recvfrom(65565)[0]
+
+        ip_header = IP(raw_buffer)
+
+        print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+# end sniffer with ctrl+c
+except KeyboardInterrupt:
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
